afe897516ee27a104d99056c54a63ee0a846063c
Revo Arya <revoparker@gmail.com>
2021-04-04 19:19:32 +0700

    backend login & register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,41 +1,93 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import Axios from "axios";
 
 class App extends Component {
-  render() {
-    return (
-      <>
-        <div className="container login-center">
-        <div className="col-10 col-md-4 col-lg-3">
-            <h1 className="loginCenter">Login</h1>
-            <div className="form-group">
-                <label for="username" className="form-head">Username or Email</label>
-                <input type="email" className="form-control" id="username" placeholder="" />
-            </div>
-            <div className="form-group">
-                <div className="password-container">
-                    <label for="password" className="form-head">Password</label>
-                    <input type="password" className="form-control" id="password" />
-                    <img src="images/icons/reveal-icon.svg" className="reveal-icon" alt="reveal" />
-                </div>
-            </div>
-            <Link to="/Reset-Password" className="forgot-pass float-end mt-3 mb-3">Forgot
-                password?</Link>
+    state = {
+        userNameOrEmail: "",
+        password: ""
+    }
 
-            <div className="d-grid gap-2 col-12 mx-auto">
-                <Link to="/User/Dashboard" className="btn btn-primary-color mb-2" type="button">Login</Link>
-                <Link to="/Fasilitator/Dashboard" className="btn btn-google" type="button">
-                    <img src="images/icons/google-icon.svg" alt="google" className="google-icon mx-1" />Login with google
-                </Link>
-            <div className="new-regist">
-                <span className="new-regist-text">New user? </span> <Link to="./Register">Register</Link>
-            </div>
-            </div>
-        </div>
-    </div>
-      </>
-    )
-  }
+    handleChange = (e) => {
+        // console.log(e.target.name, e.target.value);
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value,
+        });
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const { userNameOrEmail, password } = this.state;
+        const body = {
+            userNameOrEmail: userNameOrEmail,
+            password: password,
+        };
+        // console.log(body);
+        // const config = {
+        //     headers: {
+        //         "origin"
+        //     }
+        // }
+        Axios
+            .post("http://localhost:8000/users/api/auth/login", body)
+            .then((result) => {
+                alert("Login Succes");
+                if (result.data.success) {
+                    result.data.data[0].role_id === 1
+                    ? this.props.history.push ('/User/Dashboard')
+                    :
+                    this.props.history.push('/Fasilitator/Dashboard')
+                }
+                console.log(result.data.data[0].role_id);
+                this.setState({
+                    userNameOrEmail: "",
+                    password: ""
+                });
+            })
+            .catch((err) => {
+                alert("Login Failed");
+                console.error(err)
+            });
+    };
+
+
+    render() {
+        return (
+            <>
+                <div className="container login-center">
+                    <div className="col-10 col-md-4 col-lg-3">
+                        <h1 className="loginCenter">Login</h1>
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="form-group">
+                                <label for="userName" className="form-head">userName or Email</label>
+                                <input type="email" className="form-control" name="userNameOrEmail" onChange={this.handleChange} />
+                            </div>
+                            <div className="form-group">
+                                <div className="password-container">
+                                    <label for="password" className="form-head">Password</label>
+                                    <input type="password" className="form-control" name="password" onChange={this.handleChange} />
+                                    <img src="images/icons/reveal-icon.svg" className="reveal-icon" alt="reveal" />
+                                </div>
+                            </div>
+
+                            <Link to="/Reset-Password" className="forgot-pass float-end mt-3 mb-3">Forgot password?</Link>
+
+                            <div className="d-grid gap-2 col-12 mx-auto">
+                                <button className="btn btn-primary-color mb-2" type="submit">Login</button>
+                                <Link to="/Fasilitator/Dashboard" className="btn btn-google" type="button">
+                                    <img src="images/icons/google-icon.svg" alt="google" className="google-icon mx-1" />Login with google </Link>
+                                <div className="new-regist">
+                                    <span className="new-regist-text">New user? </span> <Link to="./Register">Register</Link>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+            </>
+        )
+    }
 }
 
 export default App
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,41 +1,91 @@
 import React, { Component } from 'react';
 
 import { Link } from "react-router-dom";
+import Axios from 'axios'
 
 class Register extends Component {
+    state = {
+        username: "",
+        email: "",
+        password: "",
+        passwordMatch: ""
+    }
+
+    handleChange = (e) => {
+        console.log(e.target.name, e.target.value);
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value,
+        });
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const { username, email, password, passwordMatch } = this.state;
+        const body = {
+            username: username,
+            email: email,
+            password: password,
+            passwordMatch: passwordMatch
+        };
+
+        Axios
+            .post("http://localhost:8000/users/api/auth/register", body)
+            .then((result) => {
+                alert("Register Complete");
+                if (result.data.success) {
+                    this.props.history.push('/')
+                }
+                console.log(result);
+                this.setState({
+                    username: "",
+                    email: "",
+                    password: "",
+                    passwordMatch: ""
+                });
+            })
+            .catch((err) => {
+                alert("Register Failed");
+                console.error(err)
+            });
+    };
+
+
+
     render() {
         return (
             <div className="container login-center">
-        <div className="col-10 col-md-4 col-lg-3">
-            <h1 className="loginCenter">Register</h1>
-            <div className="form-group">
-                <label for="username" className="form-head">Username</label>
-                <input type="username" className="form-control" id="username" />
-                <label for="email" className="form-head">Email</label>
-                <input type="email" className="form-control" id="email" />
-            </div>
-            <div className="form-group">
-                <div className="password-container">
-                    <label for="password" className="form-head">Password</label>
-                    <input type="password" className="form-control" id="password" />
-                    <img src="images/icons/reveal-icon.svg" className="reveal-icon" alt="reveal" />
-                    <label for="password" className="form-head">Confirm Password</label>
-                    <input type="password" className="form-control" id="confirm-password" />
-                    <img src="images/icons/reveal-icon.svg" className="reveal-icon" alt="reveal" />
+                <div className="col-10 col-md-4 col-lg-3">
+                    <h1 className="loginCenter">Register</h1>
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="form-group">
+                            <label for="username" className="form-head">Username</label>
+                            <input type="username" className="form-control" name="username" onChange={this.handleChange} />
+                            <label for="email" className="form-head">Email</label>
+                            <input type="email" className="form-control" name="email" onChange={this.handleChange} />
+                        </div>
+                        <div className="form-group">
+                            <div className="password-container">
+                                <label for="password" className="form-head">Password</label>
+                                <input type="password" className="form-control" name="password" onChange={this.handleChange} />
+                                <img src="images/icons/reveal-icon.svg" className="reveal-icon" alt="reveal" />
+                                <label for="password" className="form-head">Confirm Password</label>
+                                <input type="password" className="form-control" name="passwordMatch" onChange={this.handleChange} />
+                                <img src="images/icons/reveal-icon.svg" className="reveal-icon" alt="reveal" />
+                            </div>
+                        </div>
+
+                        <div className="d-grid gap-2 col-12 mx-auto m-4">
+                            <button className="btn btn-primary-color mb-2" type="submit">Register</button>
+                            <Link to="/Fasilitator/All-Schedule" className="btn btn-google" type="button">
+                                <img src="images/icons/google-icon.svg" alt="google" className="google-icon mx-1" />Register with google</Link>
+                            <div className="new-regist">
+                                <span className="new-regist-text">Already Have Account? </span> <Link to="/">Login</Link>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
-
-            <div className="d-grid gap-2 col-12 mx-auto m-4">
-                <Link to="/User/All-Schedule" className="btn btn-primary-color mb-2" type="button">Register</Link>
-                <Link to="/Fasilitator/All-Schedule" className="btn btn-google" type="button">
-                    <img src="images/icons/google-icon.svg" alt="google" className="google-icon mx-1" />Register with google
-                </Link>
-            <div className="new-regist">
-                <span className="new-regist-text">Already Have Account? </span> <Link to="/">Login</Link>
-            </div>
-            </div>
-        </div>
-    </div>
         )
     }
 }
